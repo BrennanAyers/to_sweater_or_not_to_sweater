68accc92684c3131c54cb4254db35c96035e32ff
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-26 18:49:48 -0600

    Add initial get forecast spec

---
+++ spec/requests/forecast_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Forecast API' do
+  before :each do
+
+  end
+
+  it 'sends the weather for Denver' do
+    get '/api/v1/forecast?location=denver, co'
+
+    expect(response).to be_successful
+
+    forecast = JSON.parse(response.body)
+
+    expect(forecast['currently']['temperature']).to eq('86')
+  end
+end
