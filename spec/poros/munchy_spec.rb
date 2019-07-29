d7c9e8193b6995665164321c197b46bb95a0b9b7
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-29 11:07:33 -0600

    Add Munchy PORO spec

---
+++ spec/poros/munchy_spec.rb
@@ -0,0 +1,14 @@
+#frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Munchy do
+  it 'is a basic object' do
+    info = File.read('./spec/fixtures/munchy_info.json')
+    munchy = Munchy.new(JSON.parse(info, symbolize_names: true))
+
+    expect(munchy).to be_a Munchy
+    expect(munchy.name).to eq("Kan's Kitchen")
+    expect(munchy.address).to eq("1620 S Prairie Ave, Pueblo, CO 81005")
+  end
+end
