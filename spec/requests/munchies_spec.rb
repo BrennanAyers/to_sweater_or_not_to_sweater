27757b578d02b6f8e905fd1c8f1374ef47a6fe6a
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-29 09:26:23 -0600

    Add initial /munchies endpoint spec

---
+++ spec/requests/munchies_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe 'Munchies API' do
+  it 'return 3 open restaurants in the ending city after travel' do
+    get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
+
+    expect(response).to be_successful
+
+    results = JSON.parse(response.body)
+
+    expect(results).to be_a Hash
+    expect(results[:restaurants]).to be_an Array
+    expect(results[:restaurants].count).to eq(3)
+    expect(results[:restaurants].first).to have_key :name
+    expect(results[:restaurants].first).to have_key :address
+  end
+end
