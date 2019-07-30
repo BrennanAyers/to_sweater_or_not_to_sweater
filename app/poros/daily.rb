ec57bf9ff71b1a86e7e0966049f4b070fe9d5708
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 15:34:53 -0600

    Add Daily forecast PORO for rendering in the ForecastSerializer

---
+++ app/poros/daily.rb
@@ -0,0 +1,12 @@
+class Daily
+  attr_reader :time, :icon, :precip_probability, :precip_type, :temperature_high, :temperature_low
+
+  def initialize(forecast)
+    @time = forecast[:time]
+    @icon = forecast[:icon]
+    @precip_probability = forecast[:precipProbability]
+    @precip_type = forecast[:precipType]
+    @temperature_high = forecast[:temperatureHigh]
+    @temperature_low = forecast[:temperatureLow]
+  end
+end
