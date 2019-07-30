277255383de1af0f75e62c13129e8c0142652630
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 16:47:32 -0600

    Add VCR config to RailsHelper, add VCR.use_cassette to Service specs

--- spec/services/dark_sky_spec.rb
+++ spec/services/dark_sky_spec.rb
@@ -4,29 +4,31 @@ require 'rails_helper'
 
 RSpec.describe DarkSkyService do
   it 'forecast' do
-    forecast = subject.forecast([39.7392358, -104.990251])
+    VCR.use_cassette('dark_sky_forecast') do
+      @forecast = subject.forecast([39.7392358, -104.990251])
+    end
 
-    expect(forecast).to be_a Hash
-    expect(forecast[:currently]).to be_a Hash
-    expect(forecast[:currently]).to have_key :summary
-    expect(forecast[:currently]).to have_key :icon
-    expect(forecast[:currently]).to have_key :precipProbability
-    # expect(forecast[:currently]).to have_key :precipType
-    expect(forecast[:currently]).to have_key :time
-    expect(forecast[:currently]).to have_key :temperature
-    expect(forecast[:currently]).to have_key :apparentTemperature
-    expect(forecast[:currently]).to have_key :humidity
-    expect(forecast[:currently]).to have_key :visibility
-    expect(forecast[:currently]).to have_key :ozone
+    expect(@forecast).to be_a Hash
+    expect(@forecast[:currently]).to be_a Hash
+    expect(@forecast[:currently]).to have_key :summary
+    expect(@forecast[:currently]).to have_key :icon
+    expect(@forecast[:currently]).to have_key :precipProbability
+    # expect(@forecast[:currently]).to have_key :precipType
+    expect(@forecast[:currently]).to have_key :time
+    expect(@forecast[:currently]).to have_key :temperature
+    expect(@forecast[:currently]).to have_key :apparentTemperature
+    expect(@forecast[:currently]).to have_key :humidity
+    expect(@forecast[:currently]).to have_key :visibility
+    expect(@forecast[:currently]).to have_key :ozone
 
-    expect(forecast[:hourly]).to be_a Hash
-    expect(forecast[:hourly]).to have_key :summary
-    expect(forecast[:hourly]).to have_key :icon
-    expect(forecast[:hourly]).to have_key :data
+    expect(@forecast[:hourly]).to be_a Hash
+    expect(@forecast[:hourly]).to have_key :summary
+    expect(@forecast[:hourly]).to have_key :icon
+    expect(@forecast[:hourly]).to have_key :data
 
-    expect(forecast[:daily]).to be_a Hash
-    expect(forecast[:daily]).to have_key :summary
-    expect(forecast[:daily]).to have_key :icon
-    expect(forecast[:daily]).to have_key :data
+    expect(@forecast[:daily]).to be_a Hash
+    expect(@forecast[:daily]).to have_key :summary
+    expect(@forecast[:daily]).to have_key :icon
+    expect(@forecast[:daily]).to have_key :data
   end
 end
--- spec/services/google_maps_spec.rb
+++ spec/services/google_maps_spec.rb
@@ -4,13 +4,15 @@ require 'rails_helper'
 
 RSpec.describe GoogleMapsService do
   it '#geocode' do
-    geocode = subject.geocode('denver, co')
+    VCR.use_cassette('google_maps_geocode') do
+      @geocode = subject.geocode('denver, co')
+    end
 
-    expect(geocode).to be_a Hash
-    expect(geocode[:results]).to be_an Array
-    expect(geocode[:results].first).to have_key :geometry
-    expect(geocode[:results].first[:geometry]).to have_key :location
-    expect(geocode[:results].first[:geometry][:location]).to have_key :lat
-    expect(geocode[:results].first[:geometry][:location]).to have_key :lng
+    expect(@geocode).to be_a Hash
+    expect(@geocode[:results]).to be_an Array
+    expect(@geocode[:results].first).to have_key :geometry
+    expect(@geocode[:results].first[:geometry]).to have_key :location
+    expect(@geocode[:results].first[:geometry][:location]).to have_key :lat
+    expect(@geocode[:results].first[:geometry][:location]).to have_key :lng
   end
 end
