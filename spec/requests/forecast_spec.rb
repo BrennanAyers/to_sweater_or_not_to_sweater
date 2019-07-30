edb5210095bae68ad51ecf39028daeb77965386d
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 11:49:47 -0600

    Update /forecast request spec to have real information

--- spec/requests/forecast_spec.rb
+++ spec/requests/forecast_spec.rb
@@ -11,8 +11,43 @@ describe 'Forecast API' do
 
     expect(response).to be_successful
 
-    forecast = JSON.parse(response.body)
+    forecast = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(forecast['currently']['temperature']).to eq('86')
+    expect(forecast[:id]).to eq(Time.now.to_i.to_s)
+    expect(forecast[:type]).to eq('forecast')
+    expect(forecast[:attributes][:timezone]).to eq('America/Denver')
+    expect(forecast[:attributes][:location]).to eq('Denver, CO, USA')
+
+    currently = forecast[:attributes][:currently]
+    expect(currently).to be_a Hash
+    expect(currently).to have_key(:summary)
+    expect(currently).to have_key(:icon)
+    expect(currently).to have_key(:precip_probability)
+    expect(currently).to have_key(:precip_type)
+    expect(currently).to have_key(:temperature)
+    expect(currently).to have_key(:apparent_temperature)
+    expect(currently).to have_key(:humidity)
+    expect(currently).to have_key(:uv_index)
+    expect(currently).to have_key(:visibility)
+    expect(currently).to have_key(:temperature_high)
+    expect(currently).to have_key(:temperature_low)
+
+    hourly = forecast[:attributes][:hourly]
+    expect(hourly.count).to eq(24)
+    expect(hourly.first).to have_key(:time)
+    expect(hourly.first).to have_key(:temperature)
+    expect(hourly.first).to have_key(:icon)
+    expect(hourly.last).to have_key(:time)
+    expect(hourly.last).to have_key(:temperature)
+    expect(hourly.last).to have_key(:icon)
+
+    daily = forecast[:attributes][:daily]
+    expect(daily.count).to eq(5)
+    expect(daily.first).to have_key(:time)
+    expect(daily.first).to have_key(:icon)
+    expect(daily.first).to have_key(:precip_probability)
+    expect(daily.first).to have_key(:precip_type)
+    expect(daily.first).to have_key(:temperature_high)
+    expect(daily.first).to have_key(:temperature_low)
   end
 end
