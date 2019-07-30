4f0ff1f0218750f1e0eaaf62202afaee9c0c4411
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 15:23:41 -0600

    Add Currently PORO for Forecast organization

---
+++ app/poros/currently.rb
@@ -0,0 +1,27 @@
+class Currently
+  attr_reader :summary, :icon, :precip_probability, :precip_type, :temperature, :apparent_temperature, :humidity, :uvIndex, :visibility, :temperature_high, :temperature_low
+
+  def initialize(full_forecast, daily_forecast)
+    @summary = full_forecast[:summary]
+    @icon = full_forecast[:icon]
+    @precip_probability = full_forecast[:precipProbability]
+    @precip_type = full_forecast[:precipType]
+    @temperature = full_forecast[:temperature]
+    @apparent_temperature = full_forecast[:apparentTemperature]
+    @humidity = full_forecast[:humidity]
+    @uv_index_number = full_forecast[:uvIndex]
+    @visibility = full_forecast[:visibility]
+    @temperature_high = daily_forecast[:temperatureHigh]
+    @temperature_low = daily_forecast[:temperatureLow]
+  end
+
+  def uv_index
+    case @uv_index_number
+    when 0..2; "#{@uv_index_number} (low)"
+    when 3..5; "#{@uv_index_number} (moderate)"
+    when 6..7; "#{@uv_index_number} (high)"
+    when 8..10; "#{@uv_index_number} (very high)"
+    when 11..100; "#{@uv_index_number} (extreme)"
+    end
+  end
+end
