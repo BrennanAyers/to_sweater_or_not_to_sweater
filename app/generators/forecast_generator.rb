8a13e4efd38c0616af8a06bf2d5f8584bfbd1c33
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 13:18:04 -0600

    Add initial ForecastGenerator calling on Currently, Hourly, and Daily objects

---
+++ app/generators/forecast_generator.rb
@@ -0,0 +1,26 @@
+class ForecastGenerator
+  attr_reader :id, :timezone, :location
+
+  def initialize(dark_sky_response, geocode)
+    @forecast = dark_sky_response
+    @id = Time.now.to_i
+    @timezone = dark_sky_response[:timezone]
+    @location = geocode[:formatted_address]
+  end
+
+  def currently
+    Currently.new(@forecast[:currently], @forecast[:daily][:data][0])
+  end
+
+  def hourly
+    @forecast[:hourly][:data][0..23].map do |hour|
+      Hourly.new(hour)
+    end
+  end
+
+  def daily
+    @forecast[:daily][:data][1..5].map do |day|
+      Daily.new(day)
+    end
+  end
+end
