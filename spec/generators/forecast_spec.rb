d11bab0532a7bf1a29913952a72aef384b7f313d
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 13:17:25 -0600

    Add additional Currently, Hourly, Daily specs for ForecastGenerator, Update darksky fixture JSON

--- spec/generators/forecast_spec.rb
+++ spec/generators/forecast_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe ForecastGenerator do
     expect(subject.location).to eq('Denver, CO, USA')
   end
 
+  it 'should have a Currently object' do
+    expect(subject.currently).to be_an Currently
+  end
+
+  it 'should have Hourly objects' do
+    expect(subject.hourly.count).to eq(24)
+    expect(subject.hourly.first).to be_a Hourly
+    expect(subject.hourly.last).to be_a Hourly
+  end
+
+  it 'should have Daily objects' do
+    expect(subject.daily.count).to eq(5)
+    expect(subject.daily.first).to be_a Daily
+    expect(subject.daily.last).to be_a Daily
+  end
+
 end
