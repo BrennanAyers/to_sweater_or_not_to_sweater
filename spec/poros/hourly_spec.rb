a1f5f76b7c040e6c71eafad99db0276be68cc29f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 15:38:56 -0600

    Add spec for Daily forecast PORO, add Fixture for single Daily, fix indentation on other fixture files

---
+++ spec/poros/hourly_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Hourly do
+  before :each do
+    forecast = JSON.parse(File.read('./spec/fixtures/single_hourly.json'), symbolize_names: true)
+    @hourly = Hourly.new(forecast)
+  end
+
+  it 'has attributes' do
+    expect(@hourly.time).to eq(1564466400)
+    expect(@hourly.icon).to eq('clear-night')
+    expect(@hourly.temperature).to eq(73.85)
+  end
+end
