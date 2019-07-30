655d7be1b8357dc159dcda691572b3049900cd2c
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 16:29:39 -0600

    Move services, param processing to ForecastGenerator from ForecastController

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -5,22 +5,8 @@ module Api
     # A controller to take in a forecast request, process it, and return JSON
     class ForecastController < ApplicationController
       def show
-        geocode = google_maps_service.geocode(params[:location])[:results][0]
-        lat = geocode[:geometry][:location][:lat]
-        long = geocode[:geometry][:location][:lng]
-        forecast = dark_sky_service.forecast([lat, long])
-        generated_forecast =  ForecastGenerator.new(forecast, geocode)
+        generated_forecast =  ForecastGenerator.new(params[:location])
         render json: ForecastSerializer.new(generated_forecast)
-      end
-
-      private
-
-      def google_maps_service
-        @_google_maps_service ||= GoogleMapsService.new
-      end
-
-      def dark_sky_service
-        @_dark_sky_service ||= DarkSkyService.new
       end
     end
   end
--- app/generators/forecast_generator.rb
+++ app/generators/forecast_generator.rb
@@ -5,10 +5,13 @@
 class ForecastGenerator
   attr_reader :id, :timezone, :location
 
-  def initialize(dark_sky_response, geocode)
-    @forecast = dark_sky_response
+  def initialize(location)
+    geocode = google_maps_service.geocode(location)[:results][0]
+    lat = geocode[:geometry][:location][:lat]
+    long = geocode[:geometry][:location][:lng]
+    @forecast = dark_sky_service.forecast([lat, long])
     @id = Time.now.to_i
-    @timezone = dark_sky_response[:timezone]
+    @timezone = @forecast[:timezone]
     @location = geocode[:formatted_address]
   end
 
@@ -26,5 +29,15 @@ class ForecastGenerator
     @forecast[:daily][:data][1..5].map do |day|
       Daily.new(day)
     end
+  end
+
+  private
+
+  def google_maps_service
+    @_google_maps_service ||= GoogleMapsService.new
+  end
+
+  def dark_sky_service
+    @_dark_sky_service ||= DarkSkyService.new
   end
 end
