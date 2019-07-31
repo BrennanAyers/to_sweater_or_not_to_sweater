a6070820e27545879539c5a7f3b83e9169b36a89
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-31 07:46:45 -0600

    Add edgecase for User creation request to cover an existing email

--- spec/requests/users/create_spec.rb
+++ spec/requests/users/create_spec.rb
@@ -9,7 +9,19 @@ describe 'Users Create API' do
     expect(response).to be_successful
 
     result = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(result[:api_key]).to_not eq(nil)
+  end
+
+  it 'can not create a user with an existing email' do
+    User.create!(email: 'test@test.com', password_digest: 'failure')
+    post api_v1_users_path, params: { 'email': 'test@test.com', 'password': 'password', 'password_confirmation': 'password'}
+
+    expect(response).to be_successful
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(result[:api_key]).to eq(nil)
+    expect(result[:errors][:email]).to eq(['has already been taken'])
   end
 end
