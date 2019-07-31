34a7b2c8474beb4e94ee00f8806775be8f51e3c8
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-31 08:26:54 -0600

    Add else logic for when a User tries to sign in and gets their email or password wrong

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -8,6 +8,8 @@ module Api
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
           render json: { api_key: user.api_key }
+        else
+          render json: { errors: { account: ['does not match'] } }, status: 401
         end
       end
     end
