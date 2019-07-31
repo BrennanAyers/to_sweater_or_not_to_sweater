c41daae8a6b2a382417ebe4342ac58a4c170fb97
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 19:18:09 -0600

    Add VCR to BackgroundGenerator for stability

--- spec/generators/background_spec.rb
+++ spec/generators/background_spec.rb
@@ -5,7 +5,9 @@ require 'rails_helper'
 RSpec.describe BackgroundGenerator do
 
   it 'has an id and an image' do
-    @subject = BackgroundGenerator.new('denver, co')
+    VCR.use_cassette('unsplash_search') do
+      @subject = BackgroundGenerator.new('denver, co')
+    end
 
     expect(@subject.id).to eq('denver, co background image')
     expect(@subject.image).to be_a Background
