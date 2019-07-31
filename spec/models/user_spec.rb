d3c38c941aee43887d8125c341fd47e786b3acdf
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-31 07:09:23 -0600

    Add initial User model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+#frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+
+    it { should have_secure_password }
+    it { should validate_presence_of :password }
+  end
+end
