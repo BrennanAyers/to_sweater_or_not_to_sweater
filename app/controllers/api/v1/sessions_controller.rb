dc7856d0eb1fba177c835c63a3d1c194dea53b2b
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-31 08:16:03 -0600

    Add initial #create method for SessionsController to return the Users API key if correctly signed in

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # A controller to create a session for a user, and return an API key
+    class SessionsController < ApplicationController
+      def create
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+          render json: { api_key: user.api_key }
+        end
+      end
+    end
+  end
+end
