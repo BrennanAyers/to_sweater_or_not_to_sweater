93aff8c0900fbc77134a211cf62e73e6f38bc8c1
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-31 07:56:15 -0600

    Add status codes to API V1 Users #create action on controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,9 +7,9 @@ module Api
       def create
         user = User.new(user_params)
         if user.save
-          render json: { api_key: user.api_key }
+          render json: { api_key: user.api_key }, status: 201
         else
-          render json: { errors: user.errors.messages }
+          render json: { errors: user.errors.messages }, status: 409
         end
       end
 
