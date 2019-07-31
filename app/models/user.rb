60fa341d021a65415abd0ef45a6885b30007303e
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-31 07:29:25 -0600

    Add #set_api_key before_create action on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   has_secure_password
+
+  before_create :set_api_key
+
+  private
+
+  def set_api_key
+    self.api_key = SecureRandom.urlsafe_base64.to_s
+  end
 end
