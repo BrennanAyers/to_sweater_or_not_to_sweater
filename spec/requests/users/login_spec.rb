6166cbfc26c250da7022e843d41b4aaad7cafb86
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-31 08:26:32 -0600

    Add edge case for incorrect password when Logging in for API key as a User

--- spec/requests/users/login_spec.rb
+++ spec/requests/users/login_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe 'Users Login API' do
   it 'can create a session' do
     User.create!(email: 'test@test.com', password: 'password')
-    post api_v1_sessions_path, params: { 'email': 'test@test.com', 'password': 'password' }
+    post api_v1_sessions_path, params: { email: 'test@test.com', password: 'password' }
 
     expect(response).to be_successful
 
@@ -14,4 +14,16 @@ describe 'Users Login API' do
     expect(result[:api_key]).to_not eq(nil)
   end
 
+  it 'can not create a session with an incorrect password' do
+    User.create!(email: 'test@test.com', password: 'password')
+    post api_v1_sessions_path, params: { email: 'test@test.com', password: 'failure' }
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(result[:api_key]).to eq(nil)
+    expect(result[:errors][:account]).to eq(['does not match'])
+  end
 end
