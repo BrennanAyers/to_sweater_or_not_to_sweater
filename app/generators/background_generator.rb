5dd9c36611cc487a878709a57be51c7e53dcba2f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-30 19:24:43 -0600

    Add Background.new for image ivar based on the first image UnsplashService returns

--- app/generators/background_generator.rb
+++ app/generators/background_generator.rb
@@ -6,7 +6,9 @@ class BackgroundGenerator
 
   def initialize(location)
     @id = "#{location} background image"
-    @image = unsplash_service.search(location.split(',')[0])
+    unsplash_search = unsplash_service.search(location.split(',')[0])
+    unsplash_image = unsplash_search[:results][0]
+    @image = Background.new(unsplash_image)
   end
 
   private
